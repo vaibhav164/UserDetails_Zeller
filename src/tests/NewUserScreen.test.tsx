1998c7b1ad4e88ffa37e9307e2dad5b08d0de5d4
vaibhav164 <vai1641996@gmail.com>
2025-10-14 21:53:55 +0530

    Added Test Cases for Add user

---
+++ src/tests/NewUserScreen.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import { AddUserScreen } from '../Screens/AddUser/AddUserScreen';
+// import { NewUserScreen } from './NewUserScreen';
+
+describe('NewUserScreen', () => {
+  it('renders and validates input fields', () => {
+    const mockClose = jest.fn();
+    const mockCreate = jest.fn();
+
+    const mockNavigation = { navigate: jest.fn(), goBack: jest.fn(), setOptions: jest.fn() };
+    const { getByPlaceholderText, getByTestId } = render(
+      <AddUserScreen navigation={mockNavigation} />
+    );
+
+    fireEvent.changeText(getByPlaceholderText('First Name'), 'John');
+    fireEvent.changeText(getByPlaceholderText('Last Name'), 'Doe');
+    fireEvent.changeText(getByPlaceholderText('Email'), 'john.doe@email.com');
+
+    fireEvent.press(getByTestId('userRole-Manager'));
+    fireEvent.press(getByTestId('createUserBtn'));
+
+    expect(mockCreate).toHaveBeenCalledWith({
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john.doe@email.com',
+      role: 'Manager',
+    });
+  });
+});
